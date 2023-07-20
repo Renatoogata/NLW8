6cb2a8416a6be64d9ba84e8167835fe68dc57142
Renato Ide Ogata <renatoogata95@gmail.com>
2023-07-19 22:22:08 -0300

    Aula05, web responsive, menu, about, footer, services, contact

--- main.js
+++ main.js
@@ -4,6 +4,47 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackTopTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+
+function activateMenuAtCurrentSection(section) {
+  // linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  // verificar se a seção passou da linha
+  // quais dados vou precisar?
+
+  // o topo da sessão
+  const sectionTop = section.offsetTop
+
+  // a altura da seção
+  const sectionHeight = section.offsetHeight
+
+  // o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  //verificar se a base está abaixo da linha alvo
+  // quais dados eu vou precisar?
+
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  // o final da sessão passou a linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  // limites da sessão
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute("id")
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove("active")
+  if (sectionBoundaries) {
+    menuElement.classList.add("active")
+  }
 }
 
 function showNavOnScroll() {
